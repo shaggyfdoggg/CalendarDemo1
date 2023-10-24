e5939cecec2e1e1bb06c0fc45d8f17522ee6951a
Forrest Verellen <fsverellen@gmail.com>
2023-10-24 13:54:40 -0400

    Add Application User model and corresponding login and register views

--- CalendarDemo1/Data/ApplicationDbContext.cs
+++ CalendarDemo1/Data/ApplicationDbContext.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CalendarDemo1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CalendarDemo1.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
